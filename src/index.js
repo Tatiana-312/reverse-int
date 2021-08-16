3120e335ee7e37561cd6e43cc8a9aa96270d3883
Tatiana-312 <tatyanapetrosh@gmail.com>
2021-08-16 20:55:13 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let nPositive;
+    n < 0 ? (nPositive = n * -1) : (nPositive = n);
+    let result = "";
+    let nStr = nPositive.toString();
+    for (let i = nStr.length - 1; i >= 0; i--) {
+        result = `${result}${nStr[i]}`;
+    }
+    let a = +result;
+    return a;
+};
